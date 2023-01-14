a7f8a31e8f391f51a9b442887bec47ac4b2c1438
germiscena <germiscena@mail.ru>
2023-01-14 03:21:31 +0300

    added login component

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -5,16 +5,23 @@ import { useNavigate } from "react-router";
 
 const Registration = () => {
   const navigate = useNavigate();
+  const [login, setLogin] = React.useState(false);
+  function loggin() {
+    navigate("/main");
+    setLogin(false);
+  }
   return (
     <div className='reg'>
-      <div className='reg_info'>
+      <div className={login ? "reg_info hidden" : "reg_info"}>
         <p className='reg_info_words'>
           PokemonAPI - это дипломный проект, в виде браузерной игры про покемонов.
         </p>
-        <button className='reg_info_login'>Войти как тренер</button>
+        <button onClick={() => setLogin(true)} className='reg_info_login'>
+          Войти как тренер
+        </button>
         <img src={pokemon} alt='pokemon' className='reg_info_pokemon' />
       </div>
-      <div className='reg_form'>
+      <div className={login ? "reg_form hidden" : "reg_form"}>
         <div className='reg_form_block'>
           <div className='reg_form_block_input'>
             <label>Имя персонажа</label>
@@ -42,6 +49,21 @@ const Registration = () => {
           Пропустить
         </button>
       </div>
+      {login && (
+        <div className='reg_login'>
+          <div className='reg_login_input'>
+            <label>Имя персонажа</label>
+            <input type='text'></input>
+          </div>
+          <div className='reg_login_input'>
+            <label>Пароль</label>
+            <input type='text'></input>
+          </div>
+          <button onClick={() => loggin()} className='reg_login_button'>
+            Войти
+          </button>
+        </div>
+      )}
     </div>
   );
 };
