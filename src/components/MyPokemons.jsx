cc87ce63d5efaefcb72b5cdfc2b0e65f8d923dd3
germiscena <germiscena@mail.ru>
2023-02-04 01:38:58 +0300

    fixed poke-backpack

--- src/components/MyPokemons.jsx
+++ src/components/MyPokemons.jsx
@@ -8,100 +8,39 @@ import axiosInstance from "../config/axiosInstance";
 
 const MyPokemons = () => {
   const { backpackPokemons, setBackpackPokemons } = React.useContext(AppContext);
-  const {pokemons, setPokemons } = React.useState([]);
+  const [pokemons, setPokemons] = React.useState([]);
   async function getPokemons() {
-    let data = await axiosInstance.get(`${API_URL}/Pokemon/get-user-pokemons?userId=${localStorage.getItem(`userId`)}`);
-    setPokemons(data.data);
+    let data = await axiosInstance
+      .get(`${API_URL}/Pokemon/get-user-pokemons?userId=${localStorage.getItem(`userId`)}`)
+      .then((res) => {
+        setPokemons(res.data);
+      });
   }
   React.useEffect(() => {
     getPokemons();
-    console.log("hello");
   }, []);
+
   return (
     <div className='myPokemons'>
       <div className='myPokemons_inside'>
         <h3 className='myPokemons_inside_title'>Мои покемоны</h3>
         <div className='myPokemons_inside_pokemons'>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#00{pokemons[0].pokemonRecordId} {pokemons[0].name}</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#695 Гелиолиск</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#695 Гелиолиск</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#695 Гелиолиск</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#695 Гелиолиск</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#695 Гелиолиск</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#695 Гелиолиск</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
-          <div className='myPokemons_inside_pokemons_single'>
-            <img
-              src={heleolisk}
-              alt='pokemon'
-              className='myPokemons_inside_pokemons_single_image'
-            />
-            <p className='myPokemons_inside_pokemons_single_name'>#695 Гелиолиск</p>
-            <div className='myPokemons_inside_pokemons_single_health' />
-            <div className='myPokemons_inside_pokemons_single_exp' />
-          </div>
+          {pokemons.map((item) => {
+            return (
+              <div key={item.id} className='myPokemons_inside_pokemons_single'>
+                <img
+                  src={heleolisk}
+                  alt='pokemon'
+                  className='myPokemons_inside_pokemons_single_image'
+                />
+                <p className='myPokemons_inside_pokemons_single_name'>
+                  #00{item.pokemonRecordId} {item.name}
+                </p>
+                <div className='myPokemons_inside_pokemons_single_health' />
+                <div className='myPokemons_inside_pokemons_single_exp' />
+              </div>
+            );
+          })}
         </div>
       </div>
       <button onClick={() => setBackpackPokemons()} className='myPokemons_close'>
